4083d29555208743d98189261e74c2bd83b907ae
Elder Caal <panqueto55@gmail.com>
2024-11-01 13:44:39 -0600

    se modifico la ruta del login

--- electrotech/src/Login.js
+++ electrotech/src/Login.js
@@ -11,7 +11,7 @@ function Login() {
 
     const handleLoginClick = async () => {
         try {
-            const response = await axios.post('http://localhost:3002/api/usuarios/login', {
+            const response = await axios.post('https://otraprueba01-ayd2d5f8f7fse2ge.westus-01.azurewebsites.net/api/usuarios/login', {
                 nombre,
                 credenciales
             });
